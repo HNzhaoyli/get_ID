a4a1af8f1c9e09e9a36909e8237e526aa6e4cf6f
Henuzhaoyli <55432960+Henuzhaoyli@users.noreply.github.com>
2020-07-03 09:29:48 +0800

    Create get_id.py

---
+++ get_id.py
@@ -0,0 +1,20 @@
+def get_id(img_path):
+    camera_id = []
+    labels = []
+    for path, v in img_path:
+        #filename = path.split('/')[-1]
+        filename = os.path.basename(path)
+        label = filename[0:4]
+        camera = filename.split('c')[1]
+        if label[0:2]=='-1':
+            labels.append(-1)
+        else:
+            labels.append(int(label))
+        camera_id.append(int(camera[0]))
+    return camera_id, labels
+
+gallery_path = image_datasets['gallery'].imgs
+query_path = image_datasets['query'].imgs
+
+gallery_cam,gallery_label = get_id(gallery_path)
+query_cam,query_label = get_id(query_path)
